20499c26c7797d773e66f8d174acd99405d3e541
akhileshas <82931977+akhileshas@users.noreply.github.com>
2021-11-11 17:18:01 +0530

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"618d014135c5c8001f7782af",
+				"618d014135c5c8001f7782b1"
+			]
+		},
+		"618d014135c5c8001f7782af": {
+			"id": "618d014135c5c8001f7782af",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"618d014135c5c8001f7782b1": {
+			"id": "618d014135c5c8001f7782b1",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
